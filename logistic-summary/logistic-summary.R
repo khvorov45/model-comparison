64418397b64bfdf7e1890719b59d13e106cc55cf
Arseniy Khvorov <khvorov45@gmail.com>
2020-03-10 09:08:18 +1100

    added logistic pred summary

--- logistic-summary/logistic-summary.R
+++ logistic-summary/logistic-summary.R
@@ -9,14 +9,32 @@ library(tidyverse)
 logistic_dir <- "logistic"
 logistic_summ_dir <- "logistic-summary"
 
+# Functions ===================================================================
+
+negate_lr_est <- function(dat) {
+  mutate(dat, estimate = if_else(model == "logistic", -estimate, estimate))
+}
+
+sclr_curve <- function(x, l, b0, b1) l / (1 + exp(b0 + b1 * x))
+
+calc_pred <- function(x, dat) {
+  mutate(
+    dat,
+    logTitre = x,
+    fit = sclr_curve(logTitre, lambda, beta_0, beta_logTitre)
+  )
+}
+
+save_csv <- function(dat, name) {
+  write_csv(dat, file.path(logistic_summ_dir, glue::glue("{name}.csv")))
+}
+
 # Script ======================================================================
 
 res <- read_csv(file.path(logistic_dir, "result-10000sims.csv"))
 
 summ <- res %>%
-  mutate(
-    estimate = if_else(model == "logistic", -estimate, estimate)
-  ) %>%
+  negate_lr_est() %>%
   group_by(
     term, model, true_value, nsam, theta, beta_0, beta_logTitre, par_varied
   ) %>%
@@ -28,5 +46,31 @@ summ <- res %>%
     converged = length(unique(seed)) / 1e4
   )
 
-write_csv(summ, file.path(logistic_summ_dir, "summary-10000sims.csv"))
+save_csv(summ, "summary-10000sims")
 
+# Predictions
+preds <- res %>%
+  negate_lr_est() %>%
+  select(
+    term, estimate, model, seed, nsam,
+    theta_true = theta, beta0_true = beta_0,
+    beta_logTitre_true = beta_logTitre, par_varied
+  ) %>%
+  pivot_wider(
+    names_from = "term", values_from = "estimate"
+  ) %>%
+  mutate(
+    lambda = if_else(model == "logistic", 1, 1 - 1 / (1 + exp(theta)))
+  ) %>%
+  map_dfr(
+    seq(0, 7, length.out = 101), calc_pred, .
+  ) %>%
+  group_by(
+    model, nsam, logTitre, theta_true, beta0_true, beta_logTitre_true,
+    par_varied
+  ) %>%
+  summarise(fit = list(quantile(fit, c(0.025, 0.5, 0.975)))) %>%
+  ungroup() %>%
+  unnest_wider(fit)
+
+save_csv(preds, "preds-10000sims")
