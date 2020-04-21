9fdca89c1e15ed960ac9c686d2144aae4715fb2d
Arseniy Khvorov <khvorov45@gmail.com>
2020-04-22 09:35:03 +1000

    edits to real data application

--- data-plot/kiddyvax-plot.R
+++ data-plot/kiddyvax-plot.R
@@ -42,7 +42,8 @@ lbl_virus <- function(dat) {
   dat %>%
     mutate(
       virus_lbl = factor(
-        virus, levels = c("bvic", "byam", "h1pdm", "h1seas", "h3"),
+        virus,
+        levels = c("bvic", "byam", "h1pdm", "h1seas", "h3"),
         labels = c("B Vic", "B Yam", "A H1pdm", "A H1seas", "A H3")
       )
     )
@@ -52,7 +53,8 @@ lbl_status <- function(dat) {
   dat %>%
     mutate(
       status_lbl = factor(
-        status, levels = c(0, 1), labels = c("Not infected", "Infected")
+        status,
+        levels = c(0, 1), labels = c("Not infected", "Infected")
       )
     )
 }
@@ -87,7 +89,7 @@ inf_plot <- function(kiddyvax) {
     facet_wrap(~virus_lbl, nrow = 1) +
     geom_jitter(alpha = 0.5, shape = 18) +
     geom_text(
-      data = counts, mapping = aes(status_lbl, 10240, label = n), 
+      data = counts, mapping = aes(status_lbl, 10240, label = n),
       inherit.aes = FALSE
     )
 }
@@ -99,8 +101,9 @@ swab_plot <- function(swab) {
     mutate(
       id2 = group_indices(),
       swab_result_lbl = factor(
-        swab_result, levels = c(1, 0, NA), 
-        labels = c("Positive", "Negative", "Missing"), 
+        swab_result,
+        levels = c(1, 0, NA),
+        labels = c("Positive", "Negative", "Missing"),
         exclude = NULL
       )
     ) %>%
@@ -118,7 +121,7 @@ swab_plot <- function(swab) {
     scale_color_discrete("Swab result") +
     scale_x_date(expand = c(0.01, 0.01), minor_breaks = "month") +
     scale_y_continuous(
-      expand = c(0.01, 0.01), breaks = 1:length(unique(swab$id))
+      expand = c(0.01, 0.01), breaks = seq_len(length(unique(swab$id)))
     ) +
     facet_wrap(~virus_lbl, nrow = 1) +
     geom_segment(
@@ -126,6 +129,35 @@ swab_plot <- function(swab) {
       lwd = 0.1, col = "gray50"
     ) +
     geom_point(aes(swab_date, id2, color = swab_result_lbl), shape = 17)
+}
+
+# Plots the antibody titre and infection status
+plot_counts <- function(dat) {
+  xbreaks <- c(5 * 2^(0:12))
+  ybreaks <- seq(0, 1, 0.1)
+  dat %>%
+    ggplot(aes(log(hi), inf_prop)) +
+    dark_theme_bw(verbose = FALSE) +
+    theme(
+      panel.spacing = unit(0, "null"),
+      axis.text.x = element_text(angle = 45, hjust = 1),
+      panel.grid.minor.y = element_blank()
+    ) +
+    xlab("HI titre") +
+    ylab("Infected proportion") +
+    scale_x_continuous(
+      breaks = log(xbreaks), labels = xbreaks,
+      minor_breaks = log((xbreaks + xbreaks * 2) / 2)
+    ) +
+    scale_y_continuous(labels = scales::percent_format(1)) +
+    geom_point(
+      data = dat, mapping = aes(log(hi), inf_prop), inherit.aes = FALSE,
+      shape = 18
+    ) +
+    ggrepel::geom_text_repel(
+      data = dat, mapping = aes(log(hi), inf_prop, label = ntot),
+      inherit.aes = FALSE, color = "gray50"
+    )
 }
 
 save_plot <- function(plot, name, width, height) {
@@ -141,9 +173,13 @@ save_plot <- function(plot, name, width, height) {
 
 kiddyvax <- read_kiddyvax("kiddyvaxmain")
 swab <- read_swab("kiddyvaxmain-swab")
+summ <- read_csv(file.path(data_dir, "kiddyvaxmain-summ.csv"))
+
+summ_plot <- plot_counts(filter(summ, virus == "bvic"))
+save_plot(summ_plot, "kiddyvax-main-summ", 7.5, 7.5)
 
 kiddyvax_inf <- inf_plot(kiddyvax)
-save_plot(kiddyvax_inf, "kiddyvax-main-titre", 15, 7.5)
+# save_plot(kiddyvax_inf, "kiddyvax-main-titre", 15, 7.5)
 
 kiddyvax_swab <- swab_plot(swab)
-save_plot(last_plot(), "kiddyvax-swab", 50, 65)
+# save_plot(last_plot(), "kiddyvax-swab", 50, 65)
--- fit-cox-plot/fit-cox-plot.R
+++ fit-cox-plot/fit-cox-plot.R
@@ -23,14 +23,16 @@ recode_viruses <- function(dat) {
   dat %>%
     mutate(
       virus = factor(
-        virus, levels = c("h1pdm", "bvic"), labels = c("H1N1pdm", "B Vic")
+        virus,
+        levels = c("h1pdm", "bvic"), labels = c("H1N1pdm", "B Vic")
       )
     )
 }
 
-plot_pred <- function(dat) {
+plot_pred <- function(dat, facet_by_virus = TRUE) {
   xbreaks <- c(5 * 2^(0:10))
-  dat %>% 
+  facets <- if (facet_by_virus) facet_wrap(~virus, nrow = 1) else NULL
+  dat %>%
     ggplot(aes(loghi, prot)) +
     dark_theme_bw(verbose = FALSE) +
     theme(
@@ -43,10 +45,11 @@ plot_pred <- function(dat) {
       xlim = c(log(5), log(5120)),
       ylim = c(0, 1)
     ) +
-    facet_wrap(~ virus, nrow = 1) +
+    facets +
     scale_x_continuous("HI titre", breaks = log(xbreaks), labels = xbreaks) +
     scale_y_continuous(
-      "Relative protection", breaks = seq(0, 1, 0.1),
+      "Relative protection",
+      breaks = seq(0, 1, 0.1),
       labels = scales::percent_format(1)
     ) +
     geom_ribbon(aes(ymin = prot_low, ymax = prot_high), alpha = 0.5) +
@@ -67,21 +70,23 @@ preds <- read_pred("kiddyvaxmain") %>% recode_viruses()
 preds_sophia <- read_pred("sophia")
 
 pl <- plot_pred(preds)
-save_plot(pl, "kiddyvaxmain", 12, 7.5)
+# save_plot(pl, "kiddyvaxmain", 12, 7.5)
+
+pl_bvic <- plot_pred(filter(preds, virus == "B Vic"), facet_by_virus = FALSE)
+save_plot(pl_bvic, "kiddyvaxmain-bvic", 7.5, 7.5)
 
 pl_soph_og <- plot_pred(
   filter(preds_sophia, model == "sophia") %>%
     mutate(prot_low = prot_low_wrong, prot_high = prot_high_wrong)
 )
-save_plot(pl_soph_og, "sophia-og", 12, 7.5)
+# save_plot(pl_soph_og, "sophia-og", 12, 7.5)
 
 pl_soph_ci <- plot_pred(
   filter(preds_sophia, model == "sophia")
 )
-save_plot(pl_soph_ci, "sophia-ci", 12, 7.5)
+# save_plot(pl_soph_ci, "sophia-ci", 12, 7.5)
 
 pl_soph_ci_mod <- plot_pred(
   filter(preds_sophia, model == "me")
 )
-save_plot(pl_soph_ci_mod, "sophia-ci-mod", 12, 7.5)
-
+# save_plot(pl_soph_ci_mod, "sophia-ci-mod", 12, 7.5)
