c328f655b1aa242de90a3c640fb4eb5eae04fb1f
Arseniy Khvorov <khvorov45@gmail.com>
2020-05-01 07:15:48 +1000

    changed cox plots

--- curve-cox/curve-cox.R
+++ curve-cox/curve-cox.R
@@ -1,13 +1,9 @@
 # Script to create sample Cox time data and plot it
-# Arseniy Khvorov
-# Created 2019/09/09
-# Last edit 2019/12/05
 
 library(tidyverse)
-library(ggdark) # devtools::install_github("khvorov45/ggdark")
 
 # Directories to be used later
-curve_cox_dir <- "curve-cox"
+curve_cox_dir <- here::here("curve-cox")
 
 # Functions ===================================================================
 
@@ -15,7 +11,7 @@ pl_assumption <- function(dat, top) {
   brks <- unique(dat$subject)
   dat %>%
     ggplot(aes(x = subject, y = time_end, ymin = time_start, ymax = time_end)) +
-    dark_theme_bw(verbose = FALSE) +
+    ggdark::dark_theme_bw(verbose = FALSE) +
     theme(
       legend.title = element_blank(),
       legend.position = "bottom",
@@ -28,26 +24,21 @@ pl_assumption <- function(dat, top) {
     scale_shape_manual(values = c(18, 5)) +
     coord_flip(ylim = c(0, 300), xlim = c(0, top)) +
     geom_rect(
-      aes(xmin = -10, xmax = 30, ymin = 100, ymax = 200), 
+      aes(xmin = -10, xmax = 30, ymin = 100, ymax = 200),
       alpha = 0.15, fill = "gray50", color = NA
     ) +
     geom_linerange() +
     geom_point(aes(shape = infected))
 }
 
-save_plot <- function(pl, pl_ind, dark) {
-  suff <- if_else(dark, "_dark", "_light")
-  ggsave_dark(
-    plot = pl, dark = dark,
+save_plot <- function(pl, pl_ind) {
+  ggdark::ggsave_dark(
+    plot = pl, dark = FALSE,
     filename = file.path(
-      curve_cox_dir, paste0("timeplot_", pl_ind - 1, suff, ".pdf")
+      curve_cox_dir, paste0("timeplot_", pl_ind - 1, ".pdf")
     ),
     width = 10, height = 5, units = "cm", device = "pdf"
   )
-}
-
-save_plot_dl <- function(pl, pl_ind) {
-  walk(c(TRUE, FALSE), ~ save_plot(pl, pl_ind, .x))
 }
 
 # Script ======================================================================
@@ -61,15 +52,13 @@ dat <- tibble(
 pl0 <- ggplot(
   dat, aes(x = subject, y = time_end, ymin = time_start, ymax = time_end)
 ) +
-  dark_theme_bw(verbose = FALSE) +
+  ggdark::dark_theme_bw(verbose = FALSE) +
   xlab("Subject") +
   ylab("Time, days") +
   coord_flip()
-pl0
 
 pl1 <- pl0 +
   geom_pointrange()
-pl1
 
 dat_real <- tibble(
   subject = c(rep("1", 5), "2"),
@@ -80,7 +69,6 @@ dat_real <- tibble(
 pl3 <- pl0 +
   geom_linerange(data = dat_real) +
   geom_point(data = dat)
-pl3
 
 dat_act <- tibble(
   subject = c(1, 2),
@@ -90,7 +78,6 @@ dat_act <- tibble(
 )
 
 pl_act <- pl_assumption(dat_act, 3)
-pl_act
 
 dat_act2 <- bind_rows(
   dat_act,
@@ -103,8 +90,7 @@ dat_act2 <- bind_rows(
 )
 
 pl_act2 <- pl_assumption(dat_act2, 5)
-pl_act2
 
 pls <- list(pl0, pl1, pl3, pl_act, pl_act2)
 
-iwalk(pls, save_plot_dl)
+iwalk(pls, save_plot)
